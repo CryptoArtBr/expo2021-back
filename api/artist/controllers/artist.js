7fad3da7d79780aa527e0162f50a80ff2fd773ae
Pedro Laxe <pedrolaxe@gmail.com>
2021-05-28 22:31:33 -0300

    Update Artist API
    Clean some data on API request

--- api/artist/controllers/artist.js
+++ api/artist/controllers/artist.js
@@ -1,8 +1,37 @@
 'use strict';
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
+    find: async ctx => {
+        try {
 
-module.exports = {};
+            let result = await strapi.query('artist').find()
+            let objResponse = {}
+            objResponse.data = result
+
+            objResponse.data.forEach(function(artist){ 
+                delete artist.createdAt
+                delete artist.updatedAt
+                delete artist.created_by
+                delete artist.updated_by
+                delete artist._id
+                delete artist.__v
+                delete artist.published_at
+                delete artist.links.id
+                delete artist.links._id
+                delete artist.links.__v
+            });
+
+            ctx.send(JSON.stringify(objResponse))
+            return
+
+        } catch(e) {
+            console.log(e); // error, probably in JSON.Parser
+        }
+
+        let objResponse = {}
+        objResponse.status = "fail"
+        objResponse.status_message = "error_not_found"
+        ctx.send(JSON.stringify(objResponse))
+    }
+
+};
